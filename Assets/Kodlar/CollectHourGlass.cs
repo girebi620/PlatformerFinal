2f028d7c635d6c45a51bde8403bc5c0f4cc16673
Asas99 <122436587+Asas99@users.noreply.github.com>
2025-01-14 08:10:55 +0300

    aa

---
+++ Assets/Kodlar/CollectHourGlass.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectHourGlass : MonoBehaviour
+{
+    public int Prize;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("kum saati"))
+        {
+            GameObject.FindObjectOfType<GameManager>().time += Prize;
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ Assets/Kodlar/InfoScreen.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoScreen : MonoBehaviour
+{
+    public GameObject InfoPanel;
+    public float Counter;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Counter -= Time.fixedDeltaTime;
+        if (Counter < 0)
+        {
+            InfoPanel.SetActive(false);
+        }
+    }
+
+}
